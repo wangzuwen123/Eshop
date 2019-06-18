1afc93e9cc100d0a4b9b98115d13cd2738d4b6ed
wangzuwen123 <924462326@qq.com>
2019-06-18 11:40:48 +0800

    主页面：Fragment的切换。

--- app/src/main/java/com/example/eshop/EShopMainActivity.java
+++ app/src/main/java/com/example/eshop/EShopMainActivity.java
@@ -1,19 +1,29 @@
 package com.example.eshop;
 
 import android.app.Activity;
+
+
 import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.support.annotation.IdRes;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.eshop.R;
+import com.example.eshop.base.utils.TestFragment;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
 import java.lang.ref.WeakReference;
+
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,6 +32,13 @@ import butterknife.OnClick;
 public class EShopMainActivity extends AppCompatActivity implements OnTabSelectListener {
     @BindView(R.id.bottom_bar)
     BottomBar mBottomBar;
+
+    private TestFragment mHomeFragment;
+    private TestFragment mCategoryFragment;
+    private TestFragment mCartFragment;
+    private TestFragment mMineFragment;
+
+    private Fragment mCurrentFragment;
 
 
     @Override
@@ -34,29 +51,75 @@ public class EShopMainActivity extends AppCompatActivity implements OnTabSelectL
 
     // 视图的初始化操作
     private void initView() {
+
+        //alt + enter
         // 设置导航选择的监听事件
         mBottomBar.setOnTabSelectListener(this);
     }
 
-
+    //底部导航栏某一项选择的时候触发
     @Override
     public void onTabSelected(@IdRes int tabId) {
         switch (tabId) {
             case R.id.tab_home:
-                Toast.makeText(this, "首页", Toast.LENGTH_SHORT).show();
+                if (mHomeFragment == null) {
+                    mHomeFragment = TestFragment.newInstance("HomeFragment");
+                }
+                // 切换Fragment
+                switchfragment(mHomeFragment);
+
                 break;
             case R.id.tab_category:
-                Toast.makeText(this, "分类", Toast.LENGTH_SHORT).show();
+                if (mCategoryFragment == null) {
+                    mCategoryFragment = TestFragment.newInstance("CategoryFragment");
+                }
+                switchfragment(mCategoryFragment);
                 break;
             case R.id.tab_cart:
-                Toast.makeText(this, "购物车", Toast.LENGTH_SHORT).show();
+                if (mCartFragment == null) {
+                    mCartFragment = TestFragment.newInstance("CartFragment");
+                }
+                switchfragment(mCartFragment);
                 break;
             case R.id.tab_mine:
-                Toast.makeText(this, "我的", Toast.LENGTH_SHORT).show();
+                if (mMineFragment == null) {
+                    mMineFragment = TestFragment.newInstance("MineFragment");
+                }
+                switchfragment(mMineFragment);
+
                 break;
             default:
                 throw new UnsupportedOperationException("unsupport");
 
         }
     }
+    // 作用：切换Fragment
+    private void switchfragment(Fragment target) {
+        // show hide的方式
+
+        if (mCurrentFragment==target) return;
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        if (mCurrentFragment!=null){
+            transaction.hide(mCurrentFragment);
+        }
+        if (target.isAdded()){
+            // 如果已经添加到FragmentManager里面，就展示
+            transaction.show(target);
+        }else {
+            // 为了方便找到Fragment，我们是可以设置Tag
+            String tag = ((TestFragment)target).getArgumentText();
+
+            // 添加Fragment并设置Tag
+            transaction.add(R.id.layout_container,target,tag);
+        }
+
+        transaction.commit();
+
+        mCurrentFragment=target;
+
+
+    }
 }
+
---
+++ app/src/main/java/com/example/eshop/base/utils/TestFragment.java
@@ -0,0 +1,53 @@
+package com.example.eshop.base.utils;
+
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.eshop.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Administrator on 2019/6/18/018.
+ */
+
+public class TestFragment extends Fragment {
+    private static final String FRAGMENT_TEXT = "fragment_text";
+    @BindView(R.id.text)
+    TextView mTextView;
+
+    // 对外提供一个创建方法：传递数据
+    public static TestFragment newInstance(String text){
+        TestFragment testFragment = new TestFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FRAGMENT_TEXT,text);
+        testFragment.setArguments(bundle);
+
+        return testFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_test,container,false);
+
+        ButterKnife.bind(this,view);
+        mTextView.setText(getArgumentText());
+        return view;
+    }
+
+    public String getArgumentText(){
+        return getArguments().getString(FRAGMENT_TEXT);
+    }
+}
